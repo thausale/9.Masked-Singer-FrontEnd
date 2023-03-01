139f198f2efaec0d6396d2e0ba2defece1992372
Niels Cnops <cnops_niels@hotmail.com>
2023-03-01 16:13:10 +0100

    added statuscode

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ form.addEventListener("submit", async (e) => {
 
     //Uploading the form
     await upload(formData);
-    res.send("singer added!");
+    res.status(201).send("singer added!");
     // console.log(formData);
   } catch (error) {
     console.error(error);
