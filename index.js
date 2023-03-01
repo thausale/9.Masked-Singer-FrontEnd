bd6c554ac9bcf4ec45d421187f44a275ac4598d4
Niels Cnops <cnops_niels@hotmail.com>
2023-03-01 15:52:59 +0100

    changed datatype from object to formData

--- index.js
+++ index.js
@@ -19,21 +19,34 @@ const upload = async (body) => {
 
 form.addEventListener("submit", async (e) => {
   try {
+    console.log("form submitted");
     e.preventDefault();
     const imageInput = document.querySelector("#image");
-    //   console.log(imageInput.files[0]);
-    console.log("form submitted");
-    const body = {
-      name: document.querySelector("#name").value,
-      image: imageInput.files[0],
-      place: document.querySelector("#place").value,
-      episodeCount: document.querySelector("#episodeCount").value,
-      startedEpisode: document.querySelector("#startedEpisode").value,
-      participant: document.querySelector("#participant").value,
-    };
-    await upload(body);
 
-    console.log(body);
+    //Creating the formData datatype
+    const formData = new FormData();
+
+    //Binding all the data to the formData
+    formData.append("name", document.querySelector("#name").value);
+    formData.append("image", imageInput.files[0]);
+    formData.append("place", document.querySelector("#place").value);
+    formData.append(
+      "episodeCount",
+      document.querySelector("#episodeCount").value
+    );
+    formData.append(
+      "startedEpisode",
+      document.querySelector("#startedEpisode").value
+    );
+    formData.append(
+      "participant",
+      document.querySelector("#participant").value
+    );
+
+    //Uploading the form
+    await upload(formData);
+
+    // console.log(formData);
   } catch (error) {
     console.error(error);
   }
